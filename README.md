f724a0b33832e0141875d90ee0c19be6a0bbcc87
Parra <andresparraarze@gmail.com>
2022-07-28 17:34:35 -0400

    fixes and README file

---
+++ README.md
@@ -0,0 +1,20 @@
+# Mysql-Employee-Tracker
+
+  ## Table of Contents:
+  1. [Description](#description) 
+  2. [Installation](#Installation)
+  3. [Usage](#Usage)  
+  4. [GitHub](#GitHub)
+
+## Description
+It is designed to keep track of employees and details regarding them and gives the ability to change information as needed 
+
+## Installation
+get the code, then do npm i, then you need to set the data with mysql -u root -p place password, then source db/schema.sql and then you seed it with source db/seeds.sql do node server.js and the program should be running on your terminal
+as in the video:
+
+## Usage
+use the keys to move around and selec the options youre interested in
+
+## GitHub
+andresparraarze
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const inquirer = require("inquirer");
 const mysql = require("mysql2");
 require("console.table");
-const inquirer = require("inquirer");
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -48,35 +49,35 @@ function work () {
       //then option list
       .then(function ({task}) {
         switch (task) {
-          case "View Employees":
+            case "View Employees":
             viewEmployees();
             break;
   
-          case "View Employees by Department":
+            case "View Employees by Department":
             viewEmployeeByDepartment();
             break;
         
-          case "Add Employee":
+            case "Add Employee":
             addEmployee();
             break;
   
-          case "Remove Employees":
+            case "Remove Employees":
             removeEmployees();
             break;
   
-          case "Update Employee Role":
+            case "Update Employee Role":
             updateEmployeeRole();
             break;
   
-          case "Add Role":
+            case "Add Role":
             addRole();
             break;
   
-          case "End":
+            case "End":
             connection.end();
             break;
         }
-      });
+    });
 }
 
 
@@ -107,7 +108,7 @@ function viewEmployeeByDepartment() {
     console.log("Viewing employees by department\n");
   
     var query =
-      `SELECT d.id, d.name, r.salary AS budget
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
     FROM employee e
     LEFT JOIN role r
       ON e.role_id = r.id
@@ -364,3 +365,71 @@ function promptEmployeeRole(employeeChoices, roleChoices) {
         });
     });
 }
+
+// add role to the array
+function addRole() {
+
+    var query =
+      `SELECT d.id, d.name, r.salary AS budget
+      FROM employee e
+      JOIN role r
+      ON e.role_id = r.id
+      JOIN department d
+      ON d.id = r.department_id
+      GROUP BY d.id, d.name`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      const departmentChoices = res.map(({ id, name }) => ({
+        value: id, name: `${id} ${name}`
+      }));
+  
+      console.table(res);
+      console.log("Department array!");
+  
+      promptAddRole(departmentChoices);
+    });
+  }
+  
+  function promptAddRole(departmentChoices) {
+  
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "roleTitle",
+          message: "Role title?"
+        },
+        {
+          type: "input",
+          name: "roleSalary",
+          message: "Role Salary"
+        },
+        {
+          type: "list",
+          name: "departmentId",
+          message: "Department?",
+          choices: departmentChoices
+        },
+      ])
+      .then(function (answer) {
+  
+        var query = `INSERT INTO role SET ?`
+  
+        connection.query(query, {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: answer.departmentId
+        },
+          function (err, res) {
+            if (err) throw err;
+  
+            console.table(res);
+            console.log("Role Inserted!");
+  
+            work();
+          });
+  
+      });
+  }
